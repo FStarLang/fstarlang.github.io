b74777e9efd14b29c2a8de17e38ba21295a87d81
Dzomo, the Everest Yak <everbld@microsoft.com>
2018-05-02 06:13:14 +0000

    [CI] Refresh Low* tutorial

--- lowstar/html/static/fslit.fstarjs-config.js
+++ lowstar/html/static/fslit.fstarjs-config.js
@@ -1,7 +1,7 @@
 /* global $ FSLit FStar __FSTAR_JS_CURRENT_FST_FILE_NAME__ */
 $(function() {
-    FStar.CLI.WORKER_DIRECTORY = "_static/fstar.js/";
-    FStar.IDE.WORKER_DIRECTORY = "_static/fstar.js/";
+    FStar.CLI.WORKER_DIRECTORY = "static/fstar.js/";
+    FStar.IDE.WORKER_DIRECTORY = "static/fstar.js/";
 
     if (typeof WebAssembly === "object") {
         // Only switch to the local CLI if we can actually run it
